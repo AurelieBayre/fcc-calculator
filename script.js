9701a61647377f2a6c9cd77a771d424e2257a0e4
Aurelie Bayre <a.bayre@gmail.com>
2017-10-19 11:20:42 +0200

    adding click function removing damn typo

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 
+
+document.getElementById("one").onclick = function() {clickOne()};
+function clickOne() {
+    document.getElementById("show").innerHTML = "1";
+}
+document.getElementById("two").onclick = function() {clickTwo()};
+function clickTwo() {
+    document.getElementById("show").innerHTML = "2";
+}
+document.getElementById("three").onclick = function() {clickThree()};
+function clickThree() {
+    document.getElementById("show").innerHTML = "3";
+}
+document.getElementById("four").onclick = function() {clickFour()};
+function clickFour() {
+    document.getElementById("show").innerHTML = "4";
+}
+document.getElementById("five").onclick = function() {clickFive()};
+function clickFive() {
+    document.getElementById("show").innerHTML = "5";
+}
+document.getElementById("six").onclick = function() {clickSix()};
+function clickSix() {
+    document.getElementById("show").innerHTML = "6";
+}
+document.getElementById("seven").onclick = function() {clickSeven()};
+function clickSeven() {
+    document.getElementById("show").innerHTML = "7";
+}
+document.getElementById("eight").onclick = function() {clickEight()};
+function clickEight() {
+    document.getElementById("show").innerHTML = "8";
+}
+document.getElementById("nine").onclick = function() {clickNine()};
+function clickNine() {
+    document.getElementById("show").innerHTML = "9";
+}
+
+document.getElementById("zero").onclick = function() {clickZero()};
+
+function clickZero() {
+    document.getElementById("show").innerHTML = "0";
+}
